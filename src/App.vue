e4983fc0342b65ce330056854aeccd8cc648c6da
chenmo1212 <160594753@qq.com>
2019-10-17 11:44:28 +0800

    初始化css

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,35 @@
 <template>
   <div id="app">
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
-}
+  name: "App"
+};
 </script>
 
-<style>
-@import './css/normalize.css';
+<style lang="scss">
+// 引入外部样式
+@import "./css/normalize.css";
+@import "./css/common.css";
+
+// 主题色
+$red: #fe5a5b;
+$green: #5de4c3;
+$blue: #44afe7;
+$grey: #999999;
+$black: #091314;
+
+html {
+  font-size: 20px; // 1rem = 20px;
+}
+
 #app {
   max-width: 450px;
   margin: 0 auto;
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
@@ -23,4 +37,5 @@ export default {
   background-color: #fff;
   min-height: 100vh;
 }
+
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,34 +1,30 @@
 <template>
   <div class="hello">
-    asd
+    <div class="box text-xx shadow padding-lg margin-lr-sm">12</div>
+    <div class="bg-green round cu-btn">123s</div>
+    <div class="cu-card-sm text-center">
+      asda
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  data () {
+  name: "HelloWorld",
+  data() {
     return {
-      msg: 'Welcome to Your Vue.js App'
-    }
+      msg: "Welcome to Your Vue.js App"
+    };
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+<style scoped lang="scss">
+// 引入scss
+@import "../scss/common.scss";
+
+.test {
+  margin: 2rem;
 }
 </style>
