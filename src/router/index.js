598c13e0c1b01926ae82bf33e144de079fe26adb
chenmo1212 <160594753@qq.com>
2019-10-19 13:30:44 +0800

    add lostPage(404)

--- src/router/index.js
+++ src/router/index.js
@@ -10,59 +10,63 @@ import login from '@/components/login'
 import register from '@/components/register'
 import songList from '@/components/songList'
 import player from '@/components/player'
+import lostPage from '@/components/lostPage'
 
 Vue.use(Router)
 
 export default new Router({
     routes: [{
-            path: '/',
-            name: 'HelloWorld',
-            component: HelloWorld
-        }, {
-            path: '/test',
-            name: 'test',
-            component: test
-        }, {
-            // 播放器
-            path: '/player',
-            name: 'player',
-            component: player
-        }, {
-            // 歌单广场
-            path: '/home',
-            name: 'home',
-            component: navBar
-        }, {
-            // 搜索
-            path: '/search',
-            name: 'search',
-            component: search
-        }, {
-            // 排行榜
-            path: '/rankList',
-            name: 'rankList',
-            component: rankList
-        }, {
-            // 登录
-            path: '/login',
-            name: 'login',
-            component: login
-        }, {
-            // 注册
-            path: '/register',
-            name: 'register',
-            component: register
-        }, {
-            // 歌单详情
-            path: '/songList',
-            name: 'songList',
-            component: songList
-        },
-        {
-            // 播放器
-            path: '/player',
-            name: 'player',
-            component: player
-        },
-    ]
+        path: '/',
+        // name: 'home',
+        redirect: { name: 'home' }
+    }, {
+        path: '/test',
+        name: 'test',
+        component: test
+    }, {
+        // 播放器
+        path: '/player',
+        name: 'player',
+        component: player
+    }, {
+        // 歌单广场
+        path: '/home',
+        name: 'home',
+        component: navBar
+    }, {
+        // 搜索
+        path: '/search',
+        name: 'search',
+        component: search
+    }, {
+        // 排行榜
+        path: '/rankList',
+        name: 'rankList',
+        component: rankList
+    }, {
+        // 登录
+        path: '/login',
+        name: 'login',
+        component: login
+    }, {
+        // 注册
+        path: '/register',
+        name: 'register',
+        component: register
+    }, {
+        // 歌单详情
+        path: '/songList',
+        name: 'songList',
+        component: songList
+    }, {
+        // 播放器
+        path: '/player',
+        name: 'player',
+        component: player
+    }, {
+        // 404
+        path: "*",
+        name: 'lostPage',
+        component: lostPage
+    }]
 })
